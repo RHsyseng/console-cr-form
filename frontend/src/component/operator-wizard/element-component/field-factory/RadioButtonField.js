7d727a5c7782f7dda739cd0cfc6e6a067153d5d4
Swati Kale <swkale@redhat.com>
2019-05-07 18:56:30 -0400

    SSO LDAP Radio changes
    Signed-off-by: Swati Kale <swkale@redhat.com>

--- frontend/src/component/operator-wizard/element-component/field-factory/RadioButtonField.js
+++ frontend/src/component/operator-wizard/element-component/field-factory/RadioButtonField.js
@@ -5,36 +5,56 @@ import { FormGroup, Radio } from "@patternfly/react-core";
 export class RadioButtonField {
   constructor(props) {
     this.props = props;
+    this.handleChangeRadio = this.handleChangeRadio.bind(this);
   }
 
-  doGenerateJsx() {
-    const fieldIdTrue = this.props.ids.fieldId + "-true";
-    const fieldKeyTrue = this.props.ids.fieldKey + "-true";
-    const fieldIdFalse = this.props.ids.fieldId + "-false";
-    const fieldKeyFalse = this.props.ids.fieldKey + "-false";
+  getJsx() {
     return (
       <FormGroup
-        label={this.props.fieldDef.label}
         fieldId={this.props.ids.fieldGroupId}
         key={this.props.ids.fieldGroupKey}
       >
         <Radio
-          label="Yes"
-          aria-label="radio yes"
-          id={fieldIdTrue}
-          key={fieldKeyTrue}
-          name="horizontal-radios"
-          jsonpath={this.props.fieldDef.jsonPath}
-        />
-        <Radio
-          label="No"
-          aria-label="radio no"
-          id={fieldIdFalse}
-          key={fieldKeyFalse}
-          name="horizontal-radios"
-          jsonpath={this.props.fieldDef.jsonPath}
+          key={this.props.ids.fieldKey}
+          defaultValue={this.props.fieldDef.label}
+          onChange={this.handleChangeRadio}
+          name={this.props.parentid}
+          isChecked={this.props.fieldDef.value}
+          label={this.props.fieldDef.label}
+          id={this.props.fieldDef.label}
         />
       </FormGroup>
     );
+  }
+
+  handleChangeRadio() {
+    //const value = event.currentTarget.value;
+    // this.isCheckedRadio = !this.isCheckedRadio;
+
+    let count = 0,
+      len = 0;
+    this.props.page.props.pageDef.fields.forEach(field => {
+      if (field.label === this.props.parentid) {
+        len = field.fields.length;
+        field.fields.forEach(subfield => {
+          if (subfield.label !== this.props.fieldDef.label) {
+            count = subfield.fields.length;
+            subfield.value = false;
+          } else {
+            subfield.value = true;
+          }
+        });
+      } // );
+    });
+
+    //removed
+    this.props.page.props.pageDef.fields.splice(len, count);
+
+    //  add
+    this.props.fieldDef.fields.forEach((field, i) => {
+      this.props.page.props.pageDef.fields.splice(len - 1 + i, i + 1, field);
+    });
+    //   debugger;
+    this.props.page.loadPageChildren();
   }
 }
