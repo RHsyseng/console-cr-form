cfe189c293c12affdca2569d88fe21890eed5b6b
Yu Zhao <yzha@redhat.com>
2020-02-19 13:30:19 -0500

    Modified webpack.prod.js

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "[name].[hash].bundle.js",
+    filename: "bundle.js",
     publicPath: "/"
   },
   module: {
